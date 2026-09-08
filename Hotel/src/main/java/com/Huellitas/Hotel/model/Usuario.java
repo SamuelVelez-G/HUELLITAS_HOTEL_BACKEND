2ff98393affd2e44829cb2f3e856da7785148c9d
Mjdiazri <citruss2012@hotmail.es>
2026-09-08 13:17:40 -0500

    Se termina model usuario

--- Hotel/src/main/java/com/Huellitas/Hotel/model/Usuario.java
+++ Hotel/src/main/java/com/Huellitas/Hotel/model/Usuario.java
@@ -1,5 +1,37 @@
 package com.Huellitas.Hotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private String telefono;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String contrasena;
+    @Column(name = "fecha_registro", nullable = false)
+    private LocalDateTime fechaRegistro;
+    @OneToMany(mappedBy = "usuario")
+    private List<Mascota> mascotas = new ArrayList<>();
+    @Column(nullable = false)
+    private String rol;
 
 }
