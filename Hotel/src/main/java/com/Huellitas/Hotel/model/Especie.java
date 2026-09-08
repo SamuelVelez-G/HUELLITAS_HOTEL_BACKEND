43865fe95eeb330981c73749f11de08aafcc6ef1
NatalyBarahona <nicolbarahona.6012@gmail.com>
2026-09-08 12:24:11 -0500

    Modelos Especie y Detalle reserva

--- Hotel/src/main/java/com/Huellitas/Hotel/model/Especie.java
+++ Hotel/src/main/java/com/Huellitas/Hotel/model/Especie.java
@@ -1,5 +1,29 @@
 package com.Huellitas.Hotel.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "especies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Especie {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    @OneToMany(mappedBy = "especie")
+    private List<Mascota> mascotas;
+@oneToMany(mappedBy = "especie")
+private List<Servicio> servicios;
 }
