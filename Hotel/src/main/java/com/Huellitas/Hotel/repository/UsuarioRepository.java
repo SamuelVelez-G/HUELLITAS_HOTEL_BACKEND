e0bad84b040fc6f1f0db59e957eebef544eedc36
NatalyBarahona <nicolbarahona.6012@gmail.com>
2026-09-14 13:02:28 -0500

    Guarda cambios actuales de JWT

--- Hotel/src/main/java/com/Huellitas/Hotel/repository/UsuarioRepository.java
+++ Hotel/src/main/java/com/Huellitas/Hotel/repository/UsuarioRepository.java
@@ -1,5 +1,17 @@
 package com.Huellitas.Hotel.repository;
 
 public interface UsuarioRepository {
+import com.Huellitas.Hotel.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
 
 }
